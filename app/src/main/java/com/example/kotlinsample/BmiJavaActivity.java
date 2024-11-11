09866f1435e5e29f10b3cdce50e10583e7cbc7f9
yongsulim <sulim.yong88@gmail.com>
2024-11-12 07:51:57 +0900

    클래스 자동명

--- app/src/main/java/com/example/kotlinsample/BmiJavaActivity.java
+++ app/src/main/java/com/example/kotlinsample/BmiJavaActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class BmiJavaActivity extends AppCompatActivity {
+    // 불필요한 findViewById 함수를 피하기 위해 UI 요소를 멤버 변수로 가지고 있는다
+    EditText tallField;
+    EditText weightField;
+    TextView resultLabel;
+    Button bmiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +22,20 @@ public class BmiJavaActivity extends AppCompatActivity {
 //        UI로 사용할 레이아웃 XML 파일을 지정한다.
         setContentView(R.layout.layout_view_binding);
 
-        findViewById(R.id.bmiButton).setOnClickListener(new View.OnClickListener() {
+        // 요소 멤버변수를 XML 레이아웃에서 findViewById 함수를 이용해 바인딩한다.
+        tallField = findViewById(R.id.tallField);
+        weightField = findViewById(R.id.weightField);
+        resultLabel = findViewById(R.id.resultLabel);
+        bmiButton = findViewById(R.id.bmiButton);
+
+        // bmiButton 이 클릭된 경우의 이벤트 리스너를 등록한다.
+        bmiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // tallField의 값을 읽어 온다.
-                EditText tallField = findViewById(R.id.tallField);
                 String tall = tallField.getText().toString();
 
                 // weightField의 값을 읽어 온다.
-                EditText weightField = findViewById(R.id.weightField);
                 String weight = weightField.getText().toString();
 
                 // BMI를 계산, 체중 / 키 * 키 >> 키를 cm로 입력 받았으니 100으로 나눔
@@ -33,7 +44,6 @@ public class BmiJavaActivity extends AppCompatActivity {
                 double bmi = Double.parseDouble(weight) / Math.pow(Double.parseDouble(tall) / 100.0, 2);
 
                 // 결과 bmi를 resultLabel에 보임
-                TextView resultLabel = findViewById(R.id.resultLabel);
                 resultLabel.setText("키: " + tall + ", 체중: " + weight + ", BMI + " + bmi);
             }
         });
--- app/src/main/java/com/example/kotlinsample/BmiKotlinActivity.kt
+++ app/src/main/java/com/example/kotlinsample/BmiKotlinActivity.kt
@@ -2,12 +2,36 @@ package com.example.kotlinsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinsample.databinding.LayoutViewBindingBinding // ViewBinding 클래스 가져오기
+
 
 class BmiKotlinActivity : AppCompatActivity() {
+
+    private lateinit var binding: LayoutViewBindingBinding // ViewBinding 객체 선언
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        UI로 사용할 레이아웃 XML 파일을 지정한다.
-        setContentView(R.layout.layout_view_binding)
+
+        // ViewBinding 객체 초기화 및 레이아웃 설정
+        binding = LayoutViewBindingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // bmi 버튼이 클릭된 경우 동작하는 코드를 작성한다.
+        binding.bmiButton.setOnClickListener {
+            // tallField 의 값을 읽어 온다
+            val tall = binding.tallField.text.toString().toDoubleOrNull() ?: 0.0
+
+            // weightField 의 값을 읽어 온다
+            val weight = binding.weightField.text.toString().toDoubleOrNull() ?: 0.0
+
+            // BMI를 계산, 체중 / 키 * 키 >> 키를 cm로 입력 받았으니 100으로 나눔
+            // Math.pow()는 넘겨받은 파라미터 값을 제곱해 돌려줌
+            // ex) Math.pow(2, 3)은 2의 3제곱 8을 돌려줌
+            val bmi = weight / Math.pow(tall / 100, 2.0)
+
+            // 결과 BMI를 resultLabel에 보여준다
+            binding.resultLabel.text = "키: ${binding.tallField.text}, 체중: ${binding.weightField.text}, BMI: $bmi"
+        }
     }
 }
