abb6a1ab6f07d8f88f8a4be67019269bb440e3a7
yongsulim <sulim.yong88@gmail.com>
2024-11-30 13:56:05 +0900

    자바의 정적 유틸리티

--- app/src/main/java/com/example/kotlinsample/ToastUtilJava.java
+++ app/src/main/java/com/example/kotlinsample/ToastUtilJava.java
@@ -4,12 +4,12 @@ import android.widget.Toast;
 
 public class ToastUtilJava {
     // 짧은 토스트 메세지를 보여주는 함수
-    public void toastShort(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    public static void toastShort(String message) {
+        Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     // 긴 토스트 메세지를 보여주는 함수
-    public void toastLong(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    public static void toastLong(String message) {
+        Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_LONG).show();
     }
 }
