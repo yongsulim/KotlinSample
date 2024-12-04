fb4283824956d51fe8ca18727ebcb6499267b9f4
yongsulim <sulim.yong88@gmail.com>
2024-12-04 12:29:17 +0900

    자바에서 코틀린 최상위 함수 사용

--- app/src/main/java/com/example/kotlinsample/ToastUtilJava.java
+++ app/src/main/java/com/example/kotlinsample/ToastUtilJava.java
@@ -12,4 +12,14 @@ public class ToastUtilJava {
     public static void toastLong(String message) {
         Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_LONG).show();
     }
+
+    // 파라미터에 따라 긴 토스트 메세지, 또는 짧은 토스트 메세지를 보여준다
+    public static void toast(String message, int length){
+        Toast.makeText(MainApplication.getAppContext(), message, length).show();
+    }
+
+    // 파라미터의 기본값을 사용하기 위해 메소드 오버라이딩
+    public static void toast(String message){
+        toast(message, Toast.LENGTH_SHORT);
+    }
 }
