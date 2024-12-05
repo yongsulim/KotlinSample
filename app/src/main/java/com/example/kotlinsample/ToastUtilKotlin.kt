4a6b1936a10c00aaa1edc9dfd433cc09416d54b0
yongsulim <sulim.yong88@gmail.com>
2024-12-05 13:12:25 +0900

    자바 함수의 디폴트 파라미터

--- app/src/main/java/com/example/kotlinsample/ToastUtilKotlin.kt
+++ app/src/main/java/com/example/kotlinsample/ToastUtilKotlin.kt
@@ -10,3 +10,8 @@ fun toastShort(message:String) {
 fun toastLong(message: String) {
     Toast.makeText(MainApplication.getAppContext(), message, Toast.LENGTH_LONG).show()
 }
+
+// 코틀린에서는 함수의 기본값을 지정 가능하다
+fun toast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(MainApplication.getAppContext(), message, length).show()
+}
